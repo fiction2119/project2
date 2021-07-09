6011bb0b48c4a98422ea83440262aef2640f9a22
fiction2119 <61110830+fiction2119@users.noreply.github.com>
2021-07-09 15:20:47 +0100

    models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     pass
 
@@ -11,38 +12,47 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.category}"
 
+
 class Product(models.Model):
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=300)
     url = models.ImageField(upload_to="products", blank=True)
     initial_bid = models.IntegerField()
     seller = models.CharField(max_length=56)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return f" {self.title}: {self.description}"
 
+
 class Bid(models.Model):
     bid = models.IntegerField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name="listing")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="listing")
 
     def __str__(self):
         return f"{self.bid} | {self.product}"
 
+
 class Username(models.Model):
     username = models.CharField(max_length=56)
     bids = models.ManyToManyField(Bid, blank=True, related_name="users")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="users")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="users")
 
     def __str__(self):
         return f"{self.username}"
 
+
 class Watchlist(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="list" )
-    username = models.ForeignKey(Username, on_delete=models.CASCADE, related_name="watch")
-    
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="list")
+    username = models.ForeignKey(
+        Username, on_delete=models.CASCADE, related_name="watch")
+
     def __str__(self):
         return f"{self.product}"
+
 
 class Transaction(models.Model):
     highest_bid = models.IntegerField()
@@ -51,7 +61,8 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"Winner: {self.username} Price: {self.highest_bid} {self.product}"
-    
+
+
 class Comment(models.Model):
     comment = models.CharField(max_length=250)
     username = models.ForeignKey(Username, on_delete=models.CASCADE)
